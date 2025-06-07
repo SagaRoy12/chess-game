d57fe7f3ede4475d2378061eb8788a9d504288fa
Sagar <sagarroy20027@gmail.com>
2025-06-08 01:10:06 +0530

    added renderBoard function

--- public/js/chessGame.js
+++ public/js/chessGame.js
@@ -1,3 +1,31 @@
 const socket = io();
 const chess = new Chess()
 const boardElemnt = document.querrySelector('.chessBoard')
+
+let draggedPiece = null;
+let playerRolevv = null;
+let sourceSquare =null;
+
+const renderBoard = ()=>{
+    const board = chess.board(); // create the chess board using the chess.js library   
+    boardElement.innerHTMl = "" // clear the board element
+    board.forEach((row , rowIndex)=>{
+        row.forEach((square, squareIndex)=>{
+          const sqaredBox =   document.createEmlement('div') // create a div element for each square
+            squaredBox.classList.add("square" , 
+                (rowIndex + squareIndex)%2 === 0 ? "light" : "dark" // add light or dark class based on the row and square index
+            );
+            squaredBox.dataset.row = rowIndex;        // add data attributes for row and column
+            squaredBox.dataset.column = squareIndex; // add data attributes for row and column
+            if(square){
+                const pieceElment  = document.createElement("div");
+                pieceElement.classList.add ("piece" , square.color === "w" ? "white" : "black"); // add piece class based on the color and type
+            }
+            pieceElement.innerText = ""
+            pieceElement.draggable = playerRole === square.color // make the piece draggable only if it is the player's turn
+            pieceElement.addEventListner("dragstart" , (event)=>{
+                
+            })
+        })
+    })
+}
